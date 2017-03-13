6059e77071c13b647e7db3051c09da49dd57723a
Love Kempe <love.kempe@gmail.com>
2017-03-13 21:29:21 +0100

    Extra

--- deploy.js
+++ deploy.js
@@ -15,23 +15,18 @@ const rsync = new Rsync()
 const envPrompt = {
   type: 'list',
   name: 'env',
-  message: chalk.bgBlue.white('Which environment would you like to deploy to?'),
+  message: 'Which environment would you like to deploy to?',
   choices: ['STAGE', 'PRODUCTION'],
 }
 
 const confirmPrompt = {
   type: 'list',
   name: 'bool',
-  message: chalk.bgRed.white('WARNING! Really deploy to PRODUCTION?'),
+  message: chalk.red('WARNING! Really deploy to PRODUCTION?'),
   choices: ['NO', 'YES'],
 }
 
 function main() {
-  log('This program will guide you through the deployment.', chalk.bgBlue.white)
-  startDeploy()
-}
-
-function startDeploy() {
   inquirer.prompt(envPrompt).then(function (answers) {
     if (answers.env === 'STAGE') {
       deployStage()
@@ -46,7 +41,7 @@ function confirmProduction() {
     if (answers.bool === 'YES') {
       deployProduction()
     } else if (answers.bool === 'NO') {
-      log('Aborting ...', chalk.gray)
+      log('Aborting ...')
     }
   })
 }
@@ -56,16 +51,16 @@ function deployStage() {
   rsync.destination(dest).execute(function (error, code, cmd) {
     if (error) {
       log(error, chalk.red)
-      log(`Rsync exited with code: ${code}.`, chalk.gray)
-      log(`The command was: ${cmd}.`, chalk.gray)
+      log(`Rsync exited with code: ${code}.`)
+      log(`The command was: ${cmd}.`)
     } else {
-      log('Successfully deployed to stage!', chalk.green.bold)
+      log('Successfully deployed to stage!', chalk.green)
     }
   })
 }
 
 function deployProduction() {
-  log('TODO: Production environment', chalk.gray.bold)
+  log('TODO: Production environment')
 }
 
 main()
